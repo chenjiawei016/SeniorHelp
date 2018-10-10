f4ec50aa7be5649425ac1805d5bd06ac0f056f55
josh0622 <a47877799814017@gmail.com>
2018-10-10 18:49:56 +0800

    added review.css file

--- BIT210Tutorials/js/UserComments.js
+++ BIT210Tutorials/js/UserComments.js
@@ -12,3 +12,12 @@ function starmark(item){
     }
   }
 }
+
+function myFunction(){
+  var x = document.getElementById('reviewTable');
+  if (x.style.display == "none"){
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
